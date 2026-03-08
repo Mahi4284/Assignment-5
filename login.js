02080623d2f10415616304a13bb9e9891d077a29
Mahi4284 <afmahi192@gmail.com>
2026-03-08 19:38:39 +0600

    update code

---
+++ login.js
@@ -0,0 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const errorMsg = document.getElementById('errorMsg');
+
+    if (username === 'admin' && password === 'admin123') {
+        // localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'index.html';
+    } else {
+        errorMsg.classList.remove('hidden');
+    }
+});
